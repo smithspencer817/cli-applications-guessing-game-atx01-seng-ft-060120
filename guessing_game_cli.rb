7b34cb4177e59cb1ea51f0ea3e205981801d865a
Spencer Smith <smithspencer817@gmail.com>
2020-05-13 18:21:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,8 @@
 def run_guessing_game
   num = rand(1..6)
   puts "Guess a number between 1 and 6"
-  user_num = gets.chomp.to_i
-  if num == user_num
+  user_num = gets.chomp
+  if num == user_num.to_i
     puts "You guessed the correct number!"
   elsif user_num == "exit"
     puts "Goodbye!"
